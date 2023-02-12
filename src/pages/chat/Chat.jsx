9af9eb39c6283d85fb271d48581874d1e5651ee3
Nethmi-Randi <80019485+Nethmi-Randi@users.noreply.github.com>
2023-02-12 21:43:51 +0530

    create chat page

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -5,25 +5,42 @@ import React from "react";
 
 export default function Chat() {
     return (
-        <div>
-          <Sidebar/>
-          
-      <div className="newContainer">
-          
-        <div className="topContainer">
+      //   <div>
+           
+      //     <Sidebar/>
+      //     <p></p>
+      
          
-        </div>
-        <div className="bottomContainer">
-          <div className="right">
+        
+      //   <div className="bottomContainer">
+      //     <p>hi</p>
+      //     <div className="right">
             
-          </div>
-        </div>
-      </div>
-            
-              
-                
-            
+          
+      //   </div>
+      // </div>
+      //  </div>
 
+      <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        
+        <div className="topContainer">
+          <h1>User #001</h1>
         </div>
-    )
+        {/* <div className="txtarea">
+        <textarea />
+        </div>
+         */}
+
+         <div className="txtarea">
+           <input type="text"></input>
+           <input type="button" value ="send"></input>
+         </div>
+      </div>
+    </div>
+  );
+         
+        
+    
 }
