ae1e189b8396ef2a1e8619258716469eb82caa3f
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-03-08 16:24:17 +0530

    Add Menu

--- src/pages/menu/AddMenu.jsx
+++ src/pages/menu/AddMenu.jsx
@@ -1,43 +1,37 @@
 import React from "react";
+import TopBar from "../../component/TopBar";
+import { BsPlusCircleFill } from "react-icons/bs";
 
 export default function AddMenu()
 {
     return(
         <>
-        <div className="add-menu-full-container">
-        <div className="add-menu-container">
-           <div className="add-menu-row">
-                <h4>Vegetables</h4>
-                <ol>
-                    <input type="text" className="add-menu-row-item"/>
-                    <input type="text" className="add-menu-row-item"/>
-                    <input type="text" className="add-menu-row-item"/>
-                    <input type="text" className="add-menu-row-item"/>
-                    <input type="text" className="add-menu-row-item"/>
-                </ol>
-           </div>
-           <div className="add-menu-row">
-                <h4>Stew</h4>
-                <ol>
-                    <input type="text" className="add-menu-row-item"/>
-                    <input type="text" className="add-menu-row-item"/>
-                    <input type="text" className="add-menu-row-item"/>
-                    <input type="text" className="add-menu-row-item"/>
-                    <input type="text" className="add-menu-row-item"/>
-                </ol>
-           </div>
-           <div className="add-menu-row">
-                <h4>Meat Items</h4>
-                <ol>
-                    <input type="text" className="add-menu-row-item"/>
-                    <input type="text" className="add-menu-row-item"/>
-                    <input type="text" className="add-menu-row-item"/>
-                    <input type="text" className="add-menu-row-item"/>
-                    <input type="text" className="add-menu-row-item"/>
-                </ol>
-           </div>
-        </div>
-        <button>ADD MENU</button>
+        <div className="full-container">
+            <TopBar
+                title={'Add Menu'}
+            />
+            <div className="add-menu-container">
+                <div className="add-menu-row">
+                        <h4>Vegetables</h4> 
+                        <BsPlusCircleFill size={20}/>
+                        <ol>
+                            <input type="text" className="add-menu-row-item"/>
+                        </ol>
+                </div>
+                <div className="add-menu-row">
+                        <h4>Stew</h4><BsPlusCircleFill size={20}/>
+                        <ol>
+                            <input type="text" className="add-menu-row-item"/>
+                        </ol>
+                </div>
+                <div className="add-menu-row">
+                        <h4>Meat Items</h4><BsPlusCircleFill size={20}/>
+                        <ol>
+                            <input type="text" className="add-menu-row-item"/>
+                        </ol>
+                </div>
+            </div>
+            <button>ADD MENU</button>
         </div>
         </>
     );
