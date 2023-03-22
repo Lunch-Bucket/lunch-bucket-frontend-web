0ef1640381b11f699d331fbefb768cd433b2672f
Akila Sachintha <akilasachintha@gmail.com>
2023-03-23 01:13:15 +0530

    added changes in alignment

--- src/components/header/HeaderButton.jsx
+++ src/components/header/HeaderButton.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import {BsPlusCircleFill} from "react-icons/bs";
 
 
-export default  function HeaderButton({text})
-{
-    return(
+export default function HeaderButton({text}) {
+    return (
         <div>
             <button className="button-container">
                 <BsPlusCircleFill size={20}/>
