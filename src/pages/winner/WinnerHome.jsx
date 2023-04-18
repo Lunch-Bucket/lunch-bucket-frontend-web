2efc45cba79b55a462527544cdfd7fdd6ee061ff
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-04-18 20:07:16 +0530

    Add Winner Form Page - V1

--- src/pages/winner/WinnerHome.jsx
+++ src/pages/winner/WinnerHome.jsx
@@ -12,6 +12,7 @@ import giftImg from '../../resources/images/sample/giftPizza.jpg';
 export default function WinnerHome()
 {
     const[showWinner, setShowWinner] = useState(true);
+    const[winnerForm, setWinnerForm] = useState(false);
     const[winnerDetail, seteWinnerDetail] = useState([
         {
             name: 'Dilini Kulawansa',
@@ -22,11 +23,16 @@ export default function WinnerHome()
             giftImgUrl: giftImg,
 
         }
-    ])
+    ]);
+
+    function handleSubmit()
+    {
+
+    }
     
     return(
         <>
-         <div className="container">
+         <div className="winner-container">
                 {/* Header Bar - Promotion */}
                 <div className="header-title-bar">
                     <h1 className="header-title-bar-text">
@@ -35,20 +41,20 @@ export default function WinnerHome()
                 </div>
 
                 <div className="winner-detail-content">
-                    {showWinner && 
-                        <Link to="/addWinner">
-                            <div className="add-winner-input-box">
+                    {!showWinner && 
+                    
+                            <div className="add-winner-input-box" onClick={(e)=>{setWinnerForm(true)}}>
                                 <p  className="add-winner-input-box-text">You haven't selected the winner today.</p>
                                 <h3  className="add-winner-input-box-text add">Add Today's Winner</h3>
                             </div>
-                        </Link>
+
                     }
-                    { !showWinner &&
+                    { showWinner &&
                         <>
                         { winnerDetail.map((data,id)=>(
                             <div className="show-winner-detail-content">
                                 <div className="winner-detail-content-area">
-                                    <img src={data.profileImgUrl} alt="winner image" className="winner-detail-content-img-profile" />
+                                    {/* <img src={data.profileImgUrl} alt="winner image" className="winner-detail-content-img-profile" /> */}
                                     <h4 >{data.name}</h4>
                                     <p>Order Count: {data.orderCount}</p>
                                     <p>Order Amount: {data.totalAmount}</p>
@@ -59,6 +65,37 @@ export default function WinnerHome()
                             </div>
                         ))}
                         </>
+                    }
+                    { winnerForm &&
+                        <div className="add-winner-form">
+                            <div className="add-winner-form-item">
+                                <h4 className="add-winner-form-item-label-text">User ID</h4>
+                                <input className="add-winner-form-item-input" type="text" />
+                            </div>
+
+                            <div className="add-winner-form-item">
+                                <h4 className="add-winner-form-item-label-text">Number of Meals</h4>
+                                <input className="add-winner-form-item-input" type="text" />
+                            </div>
+
+                            <div className="add-winner-form-item">
+                                <h4 className="add-winner-form-item-label-text">Prize</h4>
+                                <input className="add-winner-form-item-input" type="text" />
+                            </div>
+
+                            <div className="add-winner-form-item">
+                                <h4 className="add-winner-form-item-label-text">Cover Image</h4>
+                                <input className="add-winner-form-item-input-img" type="file" />
+                            </div>
+
+                            <div className="add-winner-form-item-btn">
+                                <button className="add-winner-form-item-btn-submit cancel" onClick={(e)=>{setWinnerForm(false)}}>Cancel</button>
+                                <button className="add-winner-form-item-btn-submit" onClick={handleSubmit}>Submit</button>
+                            </div>
+
+                            <div style={{height:'5rem'}}></div>
+                           
+                        </div>
                     }
                
                 </div>
