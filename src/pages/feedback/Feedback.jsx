5d3d496d59450ff60d5830d35fbbddfaf887ee53
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-04-19 15:02:47 +0530

    Feedback Home Page - V1

--- src/pages/feedback/Feedback.jsx
+++ src/pages/feedback/Feedback.jsx
@@ -1,69 +1,224 @@
-import React from "react";
+import React, {useState} from "react";
 import "@fontsource/poppins";
 import {FaStar} from "react-icons/fa";
 import "./FeedbackStyles.css";
 
-const data = [
-    {
-        user: '001',
-        description: 'The food was good. ',
-        rating: '4.0',
-        menu: 'abcd',
-        date: '12/02/2023'
-    },
-    {
-        user: '001',
-        description: 'The food was good.',
-        rating: '4.5',
-        menu: 'abcd',
-        date: '12/02/2023'
-    },
-    {
-        user: '001',
-        description: 'The food was good.',
-        rating: '4.5',
-        menu: 'abcd',
-        date: '12/02/2023'
-    },
-    {
-        user: '001',
-        description: 'The food was good.',
-        rating: '4.5',
-        menu: 'abcd',
-        date: '12/02/2023'
-    },
-    {
-        user: '001',
-        description: 'The food was good.',
-        rating: '4.5',
-        menu: 'abcd',
-        date: '12/02/2023'
-    },
-];
+
 
 export default function Feedback() {
+
+
+    const [showOrderDetail, setShowOrderDetail] = useState(false)
+    const [currentUser, setCurrentUser] = useState('001')
+    const [feedback, setFeedback] =  useState([
+        {
+            user: '001',
+            description: 'The food was good. ',
+            rating: '4.0',
+            date: '12/02/2023',
+            orderInfo:
+            ([
+                {
+                    menuID: '01',
+                    menuItems:  ( ["Pumpkin",'Polos','Cashew','Fish stew','Egg']),
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+                {
+                    menuID: '02',
+                    menuItems:  ( ["Dhal",'Polos','Cashew','Fish stew','Egg']),
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+                {
+                    menuID: '03',
+                    menuItems:  ( ["Cucumber",'Polos','Cashew','Fish stew','Egg']),
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+            ]),
+        },
+        {
+            user: '002',
+            description: 'The food was good.',
+            rating: '4.5',
+            date: '12/02/2023',
+            orderInfo:
+            ([
+                {
+                    menuID: '01',
+                    menuItems:  ( ["Pumpkin",'Polos','Cashew','Fish stew','Egg','Cashew','Fish stew','Egg']),
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+                {
+                    menuID: '02',
+                    menuItems:  ( ["Dhal",'Polos','Cashew','Fish stew','Egg']),
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+                {
+                    menuID: '03',
+                    menuItems:  ( ["Cucumber",'Polos','Cashew','Fish stew','Egg']),
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+            ]),
+        },
+        {
+            user: '003',
+            description: 'The food was good.',
+            rating: '4.5',
+            date: '12/02/2023',
+            orderInfo:
+            ([
+                {
+                    menuID: '01',
+                    menuItems:  ( ["Pumpkin",'Polos','Cashew','Fish stew','Egg']),
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+                {
+                    menuID: '02',
+                    menuItems:  ( ["Dhal",'Polos','Cashew','Fish stew','Egg']),
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+                {
+                    menuID: '03',
+                    menuItems:  ( ["Cucumber",'Polos','Cashew','Fish stew','Egg']),
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+            ]),
+        },
+        {
+            user: '004',
+            description: 'The food was good.',
+            rating: '4.5',
+            date: '12/02/2023',
+            orderInfo:
+            ([
+                {
+                    menuID: '01',
+                    menuItems: ["Pumpkin",'Polos','Cashew','Fish stew','Egg'],
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1600
+                
+                },
+                {
+                    menuID: '01',
+                    menuItems: ["Pumpkin",'Polos','Cashew','Fish stew','Egg'],
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+                {
+                    menuID: '01',
+                    menuItems: ["Pumpkin",'Polos','Cashew','Fish stew','Egg'],
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+                {
+                    menuID: '01',
+                    menuItems: ["Pumpkin",'Polos','Cashew','Fish stew','Egg'],
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1800
+                
+                },
+            ]),
+        },
+        {
+            user: '005',
+            description: 'The food was good.',
+            rating: '4.5',
+            date: '12/02/2023',
+            orderInfo:
+            ([
+                {
+                    menuID: '01',
+                    menuItems:  ( ["Pumpkin",'Polos','Cashew','Fish stew','Egg']),
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1500
+                
+                },
+                {
+                    menuID: '02',
+                    menuItems:  ( ["Dhal",'Polos','Cashew','Fish stew','Egg']),
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+                {
+                    menuID: '03',
+                    menuItems:  ( ["Cucumber",'Polos','Cashew','Fish stew','Egg']),
+                    orderCount: 4,
+                    mealPrice: 'Rs'+ 1400
+                
+                },
+            ]),
+        },
+    ]);
+
+    function ShowOrderHandle(user){
+        setCurrentUser(user);
+        setShowOrderDetail(!showOrderDetail);
+        console.log("Ans"+ currentUser )
+    }
+
     return (
 
-        <div className="full-container">
+        <div className="feedback-container">
             <h1 className="title">Feedbacks</h1>
             <hr/>
-            <div className="detail-container">
+            <div className="feedback-detail-content">
                 <table className="detail-table">
-                
-                    <thead>
-                
-                    </thead>
+              
                     <tbody>
-                    {data && data.map((data, index) => (
+                    {feedback.map((data, index) => (
+                        <>
                         <tr className="data-row" key={index}>
                             <td className="user">{data.user}</td>
                             <td className="description">{data.description}</td>
-                            <td className="rating">< FaStar className="starIcon"/>{data.rating}</td>
+                            <td className="rating">
+                                <FaStar className="starIcon"/>{data.rating}</td>
                             <td>
-                                <button className="menu">View Order</button>
+                                <button className="menu" onClick={(e)=>ShowOrderHandle(data.user)}>View Order</button>
                             </td>
                             <td className="date">{data.date}</td>
-                        </tr>))}
+                        </tr>
+
+                        { showOrderDetail && currentUser === (data.user) &&
+    
+                            <div className="feedback-order-detail-content">
+                            { data.orderInfo.map((item,id) => (
+                                <div className="feedback-order-detail-content-card">
+                                    <h3>{item.menuID}</h3>
+                                    <ul className="feedback-order-detail-content-card-menu">
+                                        {item.menuItems.map((singleItem,id)=>(
+                                            <li className="feedback-order-detail-content-card-menu-item">{singleItem}</li>
+                                        ))}
+                                    </ul>
+                                    <h4>{item.orderCount}</h4>
+                                    <h4>{item.mealPrice}</h4>
+                                </div>
+                            ))}
+                            </div>
+                        }
+                             
+                        
+                        </>
+                    ))}
                     </tbody>
                 </table>
             </div>
