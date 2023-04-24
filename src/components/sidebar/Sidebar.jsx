0007fdffa0e96a8729a0c372316f8fa45bbb3c32
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-04-24 22:09:39 +0530

    Side Bar - updated

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -5,21 +5,20 @@ import PATHS from "../../common/paths/paths";
 
 const Sidebar = () => {
     return (
-        <div className="sidebar-styles">
-            <div className="top">
-                <span className="logo">
+        <div className="sidebar-container">
+            <div className="sidebar-logo-container">
+                <span className="sidebar-logo">
                     <Link to={PATHS.dashboard}>LunchBucket</Link>
                 </span>
-                <hr className="line"/>
             </div>
-            <div className="center">
-                <p className="title"><NavLink to={PATHS.order}>Orders</NavLink></p>
-                <p className="title"><NavLink to={PATHS.menu}>Menu</NavLink></p>
-                <p className="title"><NavLink to={PATHS.user}>Users</NavLink></p>
-                <p className="title"><NavLink to={PATHS.promotion}>Promotions</NavLink></p>
-                <p className="title"><NavLink to={PATHS.winner}>DailyWinner</NavLink></p>
-                <p className="title"><NavLink to={PATHS.feedback}>Feedbacks</NavLink></p>
-                <p className="title"><NavLink to={PATHS.chat}>Chats</NavLink></p>
+            <div className="side-bar-page-list">
+                <p className="side-bar-page-list-item"><NavLink to={PATHS.order}>Orders</NavLink></p>
+                <p className="side-bar-page-list-item"><NavLink to={PATHS.menu}>Menu</NavLink></p>
+                <p className="side-bar-page-list-item"><NavLink to={PATHS.user}>Users</NavLink></p>
+                <p className="side-bar-page-list-item"><NavLink to={PATHS.promotion}>Promotions</NavLink></p>
+                <p className="side-bar-page-list-item"><NavLink to={PATHS.winner}>DailyWinner</NavLink></p>
+                <p className="side-bar-page-list-item"><NavLink to={PATHS.feedback}>Feedbacks</NavLink></p>
+                <p className="side-bar-page-list-item"><NavLink to={PATHS.chat}>Chats</NavLink></p>
             </div>
         </div>
     );
