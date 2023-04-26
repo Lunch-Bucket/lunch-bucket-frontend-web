3d041938873086e5fd6da267814201f4322adf33
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-04-26 20:23:38 +0530

    updated

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -2,51 +2,44 @@ import React from "react";
 import "./User.css";
 
 const data = [
+   
     {
-        userID: 'User Id',
-        deliveryAddress: 'Delivery Detail',
-        contact: 'Contact',
-        orderCount: 'Order Count',
-        orderAmount: 'Order Amount'
-    },
-
-    {
-        userID: '001',
+        userID: '002',
         deliveryAddress: '22/1, Temple Rd, Colombo 07',
         contact: '0772345674',
         orderCount: '20',
         orderAmount: '4600'
     },
     {
-        userID: '001',
+        userID: '003',
         deliveryAddress: '22/1, Temple Rd, Colombo 07',
         contact: '0772345674',
         orderCount: '20',
         orderAmount: '4600'
     },
     {
-        userID: '001',
+        userID: '004',
         deliveryAddress: '22/1, Temple Rd, Colombo 07',
         contact: '0772345674',
         orderCount: '20',
         orderAmount: '4600'
     },
     {
-        userID: '001',
+        userID: '005',
         deliveryAddress: '22/1, Temple Rd, Colombo 07',
         contact: '0772345674',
         orderCount: '20',
         orderAmount: '4600'
     },
     {
-        userID: '001',
+        userID: '006',
         deliveryAddress: '22/1, Temple Rd, Colombo 07',
         contact: '0772345674',
         orderCount: '20',
         orderAmount: '4600'
     },
     {
-        userID: '001',
+        userID: '007',
         deliveryAddress: '22/1, Temple Rd, Colombo 07',
         contact: '0772345674',
         orderCount: '20',
@@ -60,28 +53,16 @@ export default function User() {
         <div className="full-container">
             <h1>Users</h1>
             <hr/>
-            <div className="detail-container">
-                <table className="detail-table">
-                   
-                    <thead>
-                    { /*<tr>
-                            <th>User Id</th>
-                            <th>Delivery Detail</th>
-                            <th>Contact</th>
-                            <th>Order Count</th>
-                            <th>Order Amount</th>
-                        </tr>*/}
-                    </thead>
-                    <tbody>
+            <div>
+                <table className="detail-container">
                     {data.map((data, id) => (
-                        <tr className="data-row" key={id}>
-                            <td className="uid">{data.userID}</td>
-                            <td className="delivery">{data.deliveryAddress}</td>
-                            <td className="contact">{data.contact}</td>
-                            <td className="order">{data.orderCount}</td>
-                            <td className="total">{data.orderAmount}</td>
+                        <tr className="user-page-data-row" key={id}>
+                            <td className="user-page-data-row-data">{data.userID}</td>
+                            <td className="user-page-data-row-data address">{data.deliveryAddress}</td>
+                            <td className="user-page-data-row-data">{data.contact}</td>
+                            <td className="user-page-data-row-data">{data.orderCount}</td>
+                            <td className="user-page-data-row-data">{data.orderAmount}</td>
                         </tr>))}
-                    </tbody>
                 </table>
             </div>
         </div>
