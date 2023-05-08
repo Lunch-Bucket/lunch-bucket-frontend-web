4307dfb8525694ea940afe289c40058eb73a559d
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-05-08 16:42:48 +0530

    Format Code - Part1

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Chat() {
+  const[chat, setChat] = useState([
+    {
+      userID:'User'+'#001',
+      regarding:'Menu',
+      timestamp: '5:12 PM, 21-02-2023',
+      latestMessage: 'Can you please remove fish oil'
+    },
+  ]);
+
     return (
-        <div className="newContainer">
-          <div className="topContainer">
-chat
-          </div>
-          <div className="bottomContainer" >
-            <div className="right">
-              <form>
-                Chat
-              </form>
-            </div>
-          </div>
+      <div className="full-container">
+        <div className="menu-title">
+          <h1 className="menu-title-text">Chat</h1>
+          
+        </div>
+        <hr/>
         </div>
     )
 }
