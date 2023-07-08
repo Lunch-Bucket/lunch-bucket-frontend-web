5205b5449896a4638557f430db3a2f20d087ca8d
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-07-08 17:00:13 +0530

    side bar - style updated

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,17 +2,20 @@ import "./Sidebar.css";
 import "@fontsource/poppins";
 import {Link, NavLink} from "react-router-dom";
 import PATHS from "../../common/paths/paths";
+import logo from '../../resources/images/LunchBucket_Logo.png'
 
 const Sidebar = () => {
     return (
         <div className="sidebar-container">
             <div className="sidebar-logo-container">
                 <span className="sidebar-logo">
-                    <Link to={PATHS.login}>LunchBucket</Link>
+                    <Link to={PATHS.login}>
+                        <img src={logo} style={{height:'8rem', width:'8rem'}} />
+                    </Link>
                 </span>
             </div>
             <div className="side-bar-page-list">
-                <p className="side-bar-page-list-item"><NavLink to={PATHS.dashboard}>Dashboard</NavLink></p>
+                <p className="side-bar-page-list-item" style={{marginTop:'4rem'}}><NavLink to={PATHS.dashboard}>Dashboard</NavLink></p>
                 <p className="side-bar-page-list-item"><NavLink to={PATHS.order}>Orders</NavLink></p>
                 <p className="side-bar-page-list-item"><NavLink to={PATHS.menu}>Menu</NavLink></p>
                 <p className="side-bar-page-list-item"><NavLink to={PATHS.user}>Users</NavLink></p>
