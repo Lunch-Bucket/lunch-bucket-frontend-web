d92ec310f5d280de8522e958c4212753e845fba2
subhath abeysekara <subhath@liveroom.dev>
2023-07-16 01:15:58 +0530

    change_baseurl

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -1,6 +1,17 @@
 import axios from "axios";
+import { baseUrl } from "./baseUrl";
 
 export async function getFood() {
+    try {
+        const response = await axios.get(`${baseUrl}getFood`);
+        console.log('lunch menu',response.data);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function setMenu() {
     try {
         const response = await axios.get('https://78skmyfnj5.execute-api.ap-south-1.amazonaws.com/dev/getFood');
         console.log('lunch menu',response.data);
