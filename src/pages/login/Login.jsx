00c2b53478e014bcaf173f9db3956f3281d7e37f
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-07-16 20:13:49 +0530

    Login Page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import './Login.css';
+import '../../common/styles/CommonStyles.css';
+import LoginImg from '../../resources/images/loginVector.png'
 
 export default function Login()
 {
     return(
         <>
-        <div className="login-full-screen">
-            <div className="login-full-screen-left-container"></div>
-            <div className="login-full-screen-right-container"></div>
+        <div className="full-container" >
             <div className="login-form-container">
-                
+                <div className="login-form-container-left">
+                    <div className="login-form-input-content">
+                        <h2>Login</h2>
+                        <input className="login-form-input-field" type="text" placeholder="User Name" /><br/>
+                        <input className="login-form-input-field" type="text" placeholder="Password" />
+                    </div>
+                </div>
+                <div>
+                    <img src={LoginImg} alt="login vector" className="login-img" />
+                </div>
             </div>
         </div>
         </>
