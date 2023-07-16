be2490a2a97ba61bde8b3cf857b2b799700a4482
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-07-16 21:29:49 +0530

    chat updated

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -92,7 +92,7 @@ export default function Chat() {
         <div className="header-title-bar">
           <h1 className="header-title-bar-text">{strings.chat}</h1>
           <div>
-            <button className="action-bar-btn" onClick={()=>{setChatType('suggestion')}}>Suggestions</button>
+            <button className="action-bar-btn" onClick={()=>{setChatType('suggestion')}} style={{backgroundColor: chatType== 'suggestion'? '#FFEF9C' :''}}>Suggestions</button>
             <button className="action-bar-btn" onClick={()=>{setChatType('live')}}>Live Chat</button>
           </div>
         </div>
@@ -108,11 +108,18 @@ export default function Chat() {
         {chatType == 'singleChat' &&  
         <div className="chat-main-container">
          {chat.map((chat, id)=>( <>
-         {currUser == (chat.userID) &&
-         <div className="chat-card" style={{width:'auto', maxWidth:'70%'}}>
+         {currUser == (chat.userID) &&<div style={{display:'flex', flexDirection:'column'}}>
+         <div className="chat-card" style={{width:'auto'}}>
                 {chat.regarding}
+           </div>
+           <div className="chat-reply-content">
+              <input type="text" className="chat-reply-input" />
+              <button className="action-bar-btn chat-reply-send-btn">Send Reply</button>
+           </div>
            </div>}
+       
            </>))}
+           
         </div>}
 
 
