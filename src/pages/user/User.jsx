3c95bdb666fafd99ca61040832d5785535b4fc96
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-07-17 18:49:52 +0530

    handle User Level - Update v1

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -9,14 +9,15 @@ import { getUserData } from "../../services/userService";
 
 export default function User() {
 
-    const [showThreatBtn, setShowThreatBtn] = useState(true)
-    const [currUser, setCurrUser] = useState("")
     const [user, setUser] = useState([]);
+    const [level, setLevel] = useState('');
+    const [currUser, setCurruser] = useState('')
 
     async function fetchUserData() {
         try {
             const userData  = await getUserData([]);
             setUser(userData);
+            setLevel(userData.level)
 
         } catch (error) {
             console.log("Error fetching user data:", error.message);
@@ -28,10 +29,18 @@ export default function User() {
     }, []);
 
 
-    function showThreat(value)
+    function handleLevel(levelType, userID)
     {
-
-        setCurrUser(value);   
+        setCurruser(userID);
+        if(levelType == 'up'){
+            setLevel('up')
+            alert("Successfully Leveled Up!")
+        }
+        else{
+            setLevel('drop')
+            alert("Dropped the Level!")
+        }
+ 
     }
 
     return (
@@ -43,16 +52,22 @@ export default function User() {
             <hr/>
             <div>
                 <table className="detail-table">
+                    <thead className="user-page-table-row">
+                        <th className="user-page-data-row">Customer ID</th>
+                        <th className="user-page-data-row">Ordered Packet Count</th>
+                        <th className="user-page-data-row">Returned Packet Count</th>
+                        <th className="user-page-data-row" style={{marginLeft:'5rem'}}>User Level</th>
+                    </thead>
                 <tbody>
                     {user.map((dataList, id) => (
                         <tr className="user-page-table-row" >
                             <td className="user-page-data-row" style={{minWidth:'35%'}}>{(dataList.customer_id)}</td>
-                            <td className="user-page-data-row"  >{dataList.total_packets}</td>
+                            <td className="user-page-data-row" >{dataList.total_packets}</td>
                             <td className="user-page-data-row" >{dataList.balance_packets}</td>
-                            <td className="user-page-data-row" >
-                              {showThreatBtn && <button className="user-page-data-row-data-threat-btn" onClick={()=>showThreat(dataList.customer_id)}>Threat</button>}
-                              {/* {(currUser == (dataList.customer_id) && !showThreatBtn) && <button className="user-page-data-row-data-threat-btn"  >Remove from Threat</button>} */}
-                            </td>
+                            { dataList.level == 'neutral'&& <td className="user-page-data-row" >
+                              <button className="user-page-data-row-data-level-btn" onClick={()=>handleLevel('up', dataList.customer_id)}>Up</button>
+                              <button className="user-page-data-row-data-level-btn level-down" onClick={()=>handleLevel('drop', dataList.customer_id)}>Drop</button>
+                            </td>}
                         </tr>))}
                     </tbody>
                 </table>
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,13 @@ import { getUsers } from "../controllers/userController";
 export async function getUserData() {
     try {
         const result = await getUsers();
-        return await result.data.data;
+        // return await result.data.data;
+
+        return await result.data.data.map((item) => ({
+            ...item,
+           level:'neutral'
+        }));
+        
     } catch (error) {
         console.log("error:", error.message);
     }
