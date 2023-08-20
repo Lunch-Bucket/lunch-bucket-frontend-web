5d1d794a76a0960ffa1d7c41524e27d6ec1b2444
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-08-20 08:39:27 +0530

    Formik to normal form - update

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -12,55 +12,82 @@ import axios from 'axios';
 export default function Login() {
 
 
-        const validationSchema = Yup.object({
-            username: Yup.string().required('Username is required'),
-            password: Yup.string().required('Password is required'),
-        });
-        const [projectCode, setProjectCode] = useState('64a7aec4932166ca272cd176AVT60UVT4300');
       
-        const handleLogin = async (values) => {
-            try {
-              const response = await axios.post('https://fmrlw0xn6h.execute-api.ap-south-1.amazonaws.com/dev/userLogin', {
-                email: values.email,
-                password: values.password,
-                project_code: projectCode,
-              });
-        
-              const authToken = response.data.data.token;
-              console.log('Authentication Token:', authToken);
-              localStorage.setItem('authToken', authToken);
-              window.location.replace(PATHS.orderLunch); 
-            } catch (error) {
-              console.error('Login Error:', error);
-            }
-          };
+  const [projectCode, setProjectCode] = useState('64a7aec4932166ca272cd176AVT60UVT4300');
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({ email: '', password: '' });
+
+  const validationSchema = {
+    email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+    password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i,
+  };
+
+
+const handleLogin = async (event) => {
+    event.preventDefault();
+
+    let newErrors = {};
+    // if (!validationSchema.email.test(email)) {
+    //   newErrors.email = 'Invalid email';
+    // }
+    // if (!validationSchema.password.test(password)) {
+    //   newErrors.password = 'Invalid password';
+    // }
+    setErrors(newErrors);
+
+       if (Object.keys(newErrors).length === 0) {
+      try {
+        const response = await axios.post('https://fmrlw0xn6h.execute-api.ap-south-1.amazonaws.com/dev/userLogin', {
+          email: email,
+          password: password,
+          project_code: projectCode, 
+        });
+
+        const authToken = response.data.data.token;
+        localStorage.setItem('authToken', authToken);
+        window.location.replace(PATHS.orderLunch); 
+      } catch (error) {
+        console.error('Login Error:', error);
+      }
+    }
+  };
 
     return (
         <>
             <div className="full-container" >
                 <div className="login-form-container">
                     <div className="login-form-container-left">
-                        <Formik
-                            initialValues={{ email: '', password: '' }}
-                            validationSchema={validationSchema}
-                            onSubmit={handleLogin}
-                        >
-                            <Form>
-                                <div className="login-form-input-content">
-                                    <h2>Login</h2>
-                                    <div >
-                                        <Field type="email" name="email" placeholder="Username" className="login-form-input-field"/>
-                                        <ErrorMessage name="email" component="div" className="error-message" />
-                                    </div>
-                                    <div >
-                                        <Field type="password" name="password" placeholder="Password" className="login-form-input-field"/>
-                                        <ErrorMessage name="password" component="div" className="error-message" />
-                                    </div>
-                                    <br />
-                                    <button type="submit" className="login-submit-button">Submit</button>
-                                </div>
-                            </Form>
-                        </Formik>
+                <form onSubmit={handleLogin}>
+                <div className="login-form-input-content">
+                    <h2>Login</h2>
+                        <div>
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Username"
+                                className="login-form-input-field"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            {errors.email && <div className="error-message">{errors.email}</div>}
+                        </div>
+                        <div>
+                                <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                className="login-form-input-field"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            {errors.password && <div className="error-message">{errors.password}</div>}
+                        </div>
+                        <br />
+                            <button type="submit" className="login-submit-button">Submit</button>
+                        </div>
+                        </form>
                     </div>
                     <div>
                         <img src={LoginImg} alt="login vector" className="login-img" />
