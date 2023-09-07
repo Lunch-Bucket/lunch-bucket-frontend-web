75d20707c22e82640585ef2970e1d0909f17e2b7
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-09-08 01:33:53 +0530

    Chat Pages - Updated

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -8,7 +8,7 @@ import {getChatData} from '../../services/chatService'
 function Chat() {
 
   const [chat, setChat] = useState([]);
-  const [currUser, setCurrUser] = useState('');
+  const [currChat, setCurrChat] = useState('');
   const [showSingleChat, setShowSingleChat] = useState(false);
 
 
@@ -17,19 +17,19 @@ function Chat() {
     try {
       const chatList = await getChatData([]);
       setChat(chatList.data.chats);
-      console.log('chat data', chat);
+      console.log('chat data in the component', chatList);
     } catch (error) {
-      console.log('Error fetching menu data:', error.message);
+      console.log('Error fetching chat data:', error.message);
     }
   }
   useEffect(() => {
     fetchChats();
   }, []);
 
-  function showSingleChatFunc(customerID)
+  function showSingleChatFunc(chatID)
   {
     setShowSingleChat(true);
-    setCurrUser(customerID);
+    setCurrChat(chatID);
   }
 
     return (
@@ -41,14 +41,22 @@ function Chat() {
 
         <div className="chat-main-container">
       
-            {chat.map((chat, id) => (
+            {chat.map((singleChat, index) => (
               <>
-                <div className="chat-main-container-chat-item" onClick={()=>showSingleChatFunc(chat.id)}>Customer ID: {chat.id}</div>
-                <br/>
-                {(showSingleChat && (chat.id === currUser)) &&
+                <div className="chat-main-container-chat-item" style={{backgroundColor: singleChat.view_user_state === false ? '#fff7d1':'#fcea93'}} onClick={()=>showSingleChatFunc(singleChat.id)}>Chat ID: {singleChat.id}</div>
+                {(showSingleChat && (singleChat.id === currChat)) &&
                 <div className="single-chat-container">
-                    <button onClick={setShowSingleChat(false)}>close</button>
-                    <p>{chat.customer_id}</p>
+                   <div style={{display:'flex', justifyContent:'space-between'}}>
+                      <h4 style={{marginLeft:'1rem', color:'grey', borderBottom:'1px solid grey'}}>{singleChat.customer_id}</h4>
+                      <button className="chat-popup-close-btn" onClick={()=>setShowSingleChat(false)}>close</button>
+                    </div> 
+                    {singleChat.messages.map((msg,index)=>(
+                       <div className={msg.sender === 'user' ? 'chatUser' : 'chatAdmin'}>{msg.message}</div>       
+                    ))}
+                    <div className="chat-reply-content">
+                        <input type="text" className="chat-reply-input" />
+                        <button className="action-bar-btn chat-reply-send-btn">Send Reply</button>
+                    </div> 
                 </div>
                 }
               </>
@@ -69,10 +77,7 @@ function Chat() {
               }
               </>))}
           </div>  
-          <div className="chat-reply-content">
-              <input type="text" className="chat-reply-input" />
-              <button className="action-bar-btn chat-reply-send-btn">Send Reply</button>
-          </div> 
+        
         </div>} */}
 
         </div>
