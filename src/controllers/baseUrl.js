6b46c6e8092e493c20e28d9d7aea58aea7e7c009
Diniki Jayakody <jayakodyjadn.19@uom.lk>
2024-02-03 11:56:14 +0530

    Base Url Update

--- src/controllers/baseUrl.js
+++ src/controllers/baseUrl.js
@@ -1,7 +1,12 @@
 // export const baseUrl = "https://78skmyfnj5.execute-api.ap-south-1.amazonaws.com/dev/"
 // export const projectCode = "64fdb96f15be57cccddf86bbAVT60UVT8600"
 // export const expertUrl = "http://13.126.237.132:5000/prod/getReport/"
-export const baseUrl = "https://1p8cy9d7v2.execute-api.ap-south-1.amazonaws.com/dev/"
+
+// export const baseUrl = "https://1p8cy9d7v2.execute-api.ap-south-1.amazonaws.com/dev/"
+// export const loginUrl = "https://fw2svr60sl.execute-api.ap-south-1.amazonaws.com/beta/"
+
+export const baseUrl = "https://cfdpwoutv3.execute-api.ap-south-1.amazonaws.com/dev/"
 export const projectCode = "64fef5352733ffb579bdc92dAVT60UVT8600"
 export const expertUrl = "http://13.126.237.132:5000/dev/getReport/"
-export const loginUrl = "https://fw2svr60sl.execute-api.ap-south-1.amazonaws.com/beta/"
+export const loginUrl = "https://or5u8idt71.execute-api.ap-south-1.amazonaws.com/beta/"
+
