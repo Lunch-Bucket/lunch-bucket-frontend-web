5aa1269390e96ce6a117ab1f332420980eb24630
Diniki Jayakody <jayakodyjadn.19@uom.lk>
2024-02-22 13:36:55 +0530

    Session time counter update

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,7 +8,9 @@ const Sidebar = () => {
 
     const isLoggedIn= localStorage.getItem('loginStatus') === 'true';
     const [nodeEnv, setNodeEnv] = useState(false);
-
+    const tokenGeneratedTime = parseInt(localStorage.getItem("tokenGeneratedTime"), 10);
+    const tokenExpiryTime = tokenGeneratedTime + 3550000;
+    const [remainingTime, setRemainingTime] = useState(tokenExpiryTime - Date.now());
     function nodeConfig(){
         if(process.env.NODE_ENV==='development'){
             setNodeEnv(false);
@@ -19,10 +21,38 @@ const Sidebar = () => {
         nodeConfig();
     }, []);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const newRemainingTime = tokenExpiryTime - Date.now();
+            if (newRemainingTime <= 0) {
+                clearInterval(interval);
+                localStorage.setItem('loginStatus', false);
+                // window.location.replace(PATHS.login);
+            } else {
+                setRemainingTime(newRemainingTime);
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [tokenExpiryTime]);
+
+
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / (1000 * 60));
+        const seconds = Math.floor((time % (1000 * 60)) / 1000);
+        return `${minutes} min ${seconds} secs`;
+    };
+
+  
     
     return (
         <div className="sidebar-container">
             <div className="sidebar-logo-container">
+            {remainingTime > 0 &&
+                <div className="countdown-timer">
+                    <p style={{fontSize:'12px',color:'green'}}>Session ends in {formatTime(remainingTime)}</p>
+                </div>
+            }
                 {!nodeEnv &&
                     <p style={{fontSize:'10px',color:'red'}}>DEVELOPMENT</p>
                 }
@@ -41,10 +71,7 @@ const Sidebar = () => {
                 <p className="side-bar-page-list-item"><NavLink to={PATHS.chat}>Chats</NavLink></p>
                 <p className="side-bar-page-list-item"><NavLink to={PATHS.setting}>Settings</NavLink></p>
 
-                {/* <p className="side-bar-page-list-item" style={{marginTop:'4rem'}}><NavLink to={PATHS.dashboard}>Dashboard</NavLink></p>
-                <p className="side-bar-page-list-item"><NavLink to={PATHS.promotion}>Promotions</NavLink></p>
-                <p className="side-bar-page-list-item"><NavLink to={PATHS.winner}>DailyWinner</NavLink></p>
-                <p className="side-bar-page-list-item"><NavLink to={PATHS.feedback}>Feedbacks</NavLink></p> */}
+              
             </div>
             } 
         </div>
