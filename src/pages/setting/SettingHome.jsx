83eb8939ed036474c8605d727534304bc0e2685e
Diniki Jayakody <jayakodyjadn.19@uom.lk>
2024-05-24 13:41:13 +0530

    limits update - settings

--- src/pages/setting/SettingHome.jsx
+++ src/pages/setting/SettingHome.jsx
@@ -4,9 +4,10 @@ import "./SettingStyles.css";
 import strings from '../../common/strings/strings';
 import withTokenExpirationCheck from "../../tokenExpirationCheck/withTokenExpirationCheck";
 import { getLunchMenu, getDinnerMenu } from "../../services/menuService";
-import { updateMealCount, getMealCount } from "../../services/settingservice";
+import { updateMealCount } from "../../services/settingservice";
 import Popup from "../../components/Popup";
-
+import axios from "axios";
+import baseUrl from "../../controllers/baseUrl";
 
 function SettingHome() {
 
@@ -18,6 +19,9 @@ function SettingHome() {
     const [mealCount, setMealCount] = useState('');
     const [packetCountGet, setPacketCountGet] = useState({});
 
+    const [isBetweenLunch, setIsBetweenLunch] = useState(false); //Time check
+    const [isBetweenDinner, setIsBetweenDinner] = useState(false); 
+
     const [showPopup, setShowPopup] = useState(false);
     const [popupType, setPopupType] = useState('');
     const [popupMessage, setPopupMessage] = useState('');
@@ -28,45 +32,57 @@ function SettingHome() {
         setShowPopup(true);
       };
 
-      const [formData, setFormData] = useState({
-        meal_type: '',
-        order_type: '',
-        id: '',
-      });
     
+    const [limitsLunch, setLimitsLunch] = useState({});
+    const [limitsDinner, setLimitsDinner] = useState({});
 
-    // Lunch
-    const [meatMenuLunch, setMeatMenuLunch] = useState([]);
-    const [vegeMenuLunch, setVegeMenuLunch] = useState([]);
-    const [stewMenuLunch, setStewMenuLunch] = useState([]);
     const [specialMenuLunch, setSpecialMenuLunch] = useState([]);
-    const [riceMenuLunch, setRiceMenuLunch] = useState([]);
-   
-    // Dinner
-    const [meatMenuDinner, setMeatMenuDinner] = useState([]);
-    const [vegeMenuDinner, setVegeMenuDinner] = useState([]);
-    const [stewMenuDinner, setStewMenuDinner] = useState([]);
-    const [specialMenuDinner, setSpecialMenuDinner] = useState([]);
-    const [riceMenuDinner, setRiceMenuDinner] = useState([]);
+    const [specialMenuDinner, setSpecialMenuDinner] = useState([]);   
 
     const [navOnline,setNavOnline] = useState(true)
 
 
     async function fetchMenu() {
         try {
+            //Lunch
             const menuListLunch = await getLunchMenu([]);
-            const menuListDinner = await getDinnerMenu([]);
-            setVegeMenuLunch(menuListLunch.data.vege_menu_lunch);
-            setMeatMenuLunch(menuListLunch.data.meat_menu_lunch);
-            setStewMenuLunch(menuListLunch.data.stew_menu_lunch);
-            setRiceMenuLunch(menuListLunch.data.rice_menu_lunch);
-            setSpecialMenuLunch(menuListLunch.data.special_menu_lunch);
+         
+            const initialLimits = {};
+            
+            menuListLunch.data.vege_menu_lunch.forEach(item => initialLimits[item.type] = item.limit || 0);
+            menuListLunch.data.meat_menu_lunch.forEach(item => initialLimits[item.type] = item.limit || 0);
+            menuListLunch.data.stew_menu_lunch.forEach(item => initialLimits[item.type] = item.limit || 0);
+            menuListLunch.data.rice_menu_lunch.forEach(item => initialLimits[item.type] = item.limit || 0);
+            
+        if (Array.isArray(menuListLunch.data.special_menu_lunch)) {
+            menuListLunch.data.special_menu_lunch.forEach(category => {
+              if (Array.isArray(category)) {
+                category.forEach(item => initialLimits[item.type] = item.limit || 0);
+              }
+            });
+          }
+          setLimitsLunch(initialLimits);
+  
 
-            setVegeMenuDinner(menuListDinner.data.vege_menu_dinner);
-            setMeatMenuDinner(menuListDinner.data.meat_menu_dinner);
-            setStewMenuDinner(menuListDinner.data.stew_menu_dinner);
-            setRiceMenuDinner(menuListDinner.data.rice_menu_dinner);
-            setSpecialMenuDinner(menuListDinner.data.special_menu_dinner);
+          //Dinner
+          const menuListDinner = await getDinnerMenu([]);
+
+          const initialLimitsDinner = {};
+
+          menuListDinner.data.vege_menu_dinner.forEach(itemDinner => initialLimitsDinner[itemDinner.type] = itemDinner.limitDinner || 0);
+          menuListDinner.data.meat_menu_dinner.forEach(itemDinner => initialLimitsDinner[itemDinner.type] = itemDinner.limitDinner || 0);
+          menuListDinner.data.stew_menu_dinner.forEach(itemDinner => initialLimitsDinner[itemDinner.type] = itemDinner.limitDinner || 0);
+          menuListDinner.data.rice_menu_dinner.forEach(itemDinner => initialLimitsDinner[itemDinner.type] = itemDinner.limitDinner || 0);
+          
+      if (Array.isArray(menuListDinner.data.special_menu_dinner)) {
+        menuListDinner.data.special_menu_dinner.forEach(category => {
+            if (Array.isArray(category)) {
+              category.forEach(itemDinner => initialLimitsDinner[itemDinner.type] = itemDinner.limit || 0);
+            }
+          });
+        }
+
+            setLimitsDinner(initialLimitsDinner);
   
 
         } catch (error) {
@@ -74,14 +90,6 @@ function SettingHome() {
         }
       }
 
-    //   const fetchPacketCount = async () => {
-    //     try {
-    //         const packetCountDetail = await getMealCount(meal_type, order_type, id);
-    //         setPacketCountGet(packetCountDetail)
-    //     } catch (error) {
-    //         console.log('Error fetching packet count data:', error.message);
-    //       }
-    //     }
         const data = packetCountGet.data || {}; 
 
         useEffect(() => {
@@ -99,11 +107,68 @@ function SettingHome() {
         }
        }, []);
 
-    //    useEffect(() => {
-    //     if(navOnline){
-    //         fetchPacketCount();
-    //     }
-    //     }, []);
+//reduced api calls by making two functions
+       const handleUpdateLimitsLunch = async () => {
+        try {
+          const formattedLimits = {
+            meal_type: 'Lunch', 
+            limits: { ...limitsLunch } 
+          };
+      
+          const response = await controllerUpdateLimits(formattedLimits);
+          console.log('Response from updateLimits:', response);
+        } catch (error) {
+          console.error('Error updating limits:', error);
+
+        }
+      };
+
+      const handleUpdateLimitsDinner = async () => {
+        try {
+          const formattedLimits = {
+            meal_type: 'Dinner', 
+            limits: { ...limitsDinner } 
+          };
+      
+          const response = await controllerUpdateLimits(formattedLimits);
+          console.log('Response from updateLimits:', response);
+        } catch (error) {
+          console.error('Error updating limits:', error);
+
+        }
+      };
+
+
+    //Update Limits
+   async function controllerUpdateLimits({meal_type, limits}) {
+    try {
+      const token = localStorage.getItem('lb_auth_token');
+      const response = await axios.put(`${baseUrl}freeorder_updatelimit`, {
+        meal_type,
+        limits
+      }, { headers: { 'token': `${token}` }});
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+      
+    
+      const handleLimitChangeLunch = (key, value) => {
+        const numericValue = parseInt(value, 10);
+        setLimitsLunch(prevLimits => ({
+          ...prevLimits,
+          [key]: numericValue
+        }));
+      };
+
+      const handleLimitChangeDinner = (key, value) => {
+        const numericValue = parseInt(value, 10);
+        setLimitsDinner(prevLimits => ({
+          ...prevLimits,
+          [key]: numericValue
+        }));
+      };
 
 
 
@@ -154,8 +219,37 @@ function SettingHome() {
             }
           };
 
-    
-       
+          //Time checker
+          useEffect(() => {
+            const checkTime = () => {
+              const now = new Date();
+             
+              //Lunch
+              const startTimeLunch = new Date();
+              const endTimeLunch = new Date();
+
+              startTimeLunch.setHours(10, 0, 0, 0); // 10:00
+              endTimeLunch.setHours(11, 0, 0, 0);   // 11:00
+
+              setIsBetweenLunch(now >= startTimeLunch && now <= endTimeLunch);
+
+              //Dinner
+              const startTimeDinner = new Date();
+              const endTimeDinner = new Date();
+
+              startTimeDinner.setHours(18, 0, 0, 0); // 18:00
+              endTimeDinner.setHours(19, 0, 0, 0);   // 19:00
+      
+              setIsBetweenDinner(now >= startTimeDinner && now <= endTimeDinner);
+            };
+        
+            checkTime();
+            const interval = setInterval(checkTime, 60000); // Check every minute
+        
+            return () => clearInterval(interval); // Cleanup interval on component unmount
+          }, []);
+
+
 
     return (<>
         <div className="full-container">
@@ -208,67 +302,76 @@ function SettingHome() {
             {showMenu &&
             <div style={{display:'flex', justifyContent:'space-around', margin:'3rem'}}>
                 <div>
-                    <h3>Lunch - Choice</h3>
-                    {riceMenuLunch.map((item,id)=>(
-                        <ul>
-                            <li>{item.type}</li>
-                        </ul>
-                    ))}
+                {isBetweenLunch ? (
+                    <div>
+                       <h3>Lunch - Choice</h3>
+                            {Object.keys(limitsLunch).map((key, index) => (
+                              <div key={index} style={{display:'flex',justifyContent:'space-between', margin:'1rem'}}>
+                              <label>{key}</label>
+                              <input
+                                  style={{width:'3rem'}}
+                                  type="number"
+                                  value={limitsLunch[key]}
+                                  onChange={(e) => handleLimitChangeLunch(key, e.target.value)}
+                              />
+                              </div>
+                          ))}
+                           <button onClick={handleUpdateLimitsLunch}>Update Choice Limits</button>
                     <hr/>
-                    {vegeMenuLunch.map((item,id)=>(
-                        <ul>
-                            <li>{item.type}</li>
+                        <h3>Lunch - Special</h3>
+                        {specialMenuLunch.map((item,id)=>(
+                            <ul>
+                            {item.category.map((data,index)=>(
+                                <li>{data.type}</li>
+                            ))}
+                            
                         </ul>
-                    ))}
+                        ))}
+                          <button onClick={null}>Update Special Limits</button>
+                    </div>
+                  ) : (
+                    <div>
+                      <p style={{maxWidth:'15rem', color:'red'}}>Update Lunch Limits in Between 10.00am - 11.00am</p>
+                       <h3>Lunch - Choice</h3>
+                        {Object.keys(limitsLunch).map((key, index) => (
+                            <div key={index} style={{display:'flex',justifyContent:'space-between', margin:'1rem'}}>
+                            <label>{key}</label>
+                            </div>
+                        ))}
                     <hr/>
-                    {meatMenuLunch.map((item,id)=>(
-                        <ul>
-                            <li>{item.type}</li>
-                        </ul>
-                    ))}
-                    <hr/>
-                    {stewMenuLunch.map((item,id)=>(
-                        <ul>
-                            <li>{item.type}</li>
-                        </ul>
-                    ))}
-                    <hr/>
-                    <h3>Lunch - Special</h3>
-                    {specialMenuLunch.map((item,id)=>(
-                        <ul>
-                            {item.category.map((data,id)=>(
+                        <h3>Lunch - Special</h3>
+                        {specialMenuLunch.map((item,id)=>(
+                            <ul>
+                            {item.category.map((data,index)=>(
                                 <li>{data.type}</li>
                             ))}
                             
                         </ul>
                     ))}
+                    </div>
+                  )}
+                   
                 </div>
 
+
                 <div>
+                {isBetweenDinner ? (
+                    <div>
                     <h3>Dinner - Choice</h3>
-                    {riceMenuDinner.map((item,id)=>(
-                        <ul>
-                            <li>{item.type}</li>
-                        </ul>
+                    {Object.keys(limitsDinner).map((key, index) => (
+                        
+                        <div key={index} style={{display:'flex',justifyContent:'space-between', margin:'1rem'}}>
+                        <label>{key}</label>
+                        <input
+                            style={{width:'3rem'}}
+                            type="number"
+                            value={limitsDinner[key]}
+                            onChange={(e) => handleLimitChangeDinner(key, e.target.value)}
+                        />
+                        </div>
                     ))}
-                    <hr/>
-                    {vegeMenuDinner.map((item,id)=>(
-                        <ul>
-                            <li>{item.type}</li>
-                        </ul>
-                    ))}
-                    <hr/>
-                    {meatMenuDinner.map((item,id)=>(
-                        <ul>
-                            <li>{item.type}</li>
-                        </ul>
-                    ))}
-                    <hr/>
-                    {stewMenuDinner.map((item,id)=>(
-                        <ul>
-                            <li>{item.type}</li>
-                        </ul>
-                    ))}
+                      <button onClick={handleUpdateLimitsDinner}>Update Choice Limits</button>
+
                     <hr/>
                     <h3>Dinner - Special</h3>
                     {specialMenuDinner.map((item,id)=>(
@@ -279,9 +382,33 @@ function SettingHome() {
                          
                      </ul>
                     ))}
+                    <button onClick={null}>Update Special Limits</button>
+
+                </div> ) : (
+                   <div>
+                    <p style={{maxWidth:'15rem', color:'red'}}>Update Dinner Limits in Between 6.00pm - 7.00pm</p>
+                   <h3>Dinner - Choice</h3>
+                      {Object.keys(limitsDinner).map((key, index) => (
+                          <div key={index} style={{display:'flex',justifyContent:'space-between', margin:'1rem'}}>
+                          <label>{key}</label>
+                          </div>
+                ))}
+                <hr/>
+                    <h3>Dinner - Special</h3>
+                    {specialMenuDinner.map((item,id)=>(
+                        <ul>
+                        {item.category.map((data,index)=>(
+                            <li>{data.type}</li>
+                        ))}
+                        
+                    </ul>
+                    ))}
                 </div>
+              )}
             </div>
-            }
+            
+            </div>}
+
             {showPopup && (
               <Popup type={popupType} message={popupMessage} onClose={() => setShowPopup(false)} />
             )}
