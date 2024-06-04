8c1acbe8950714355ced4b2a738ffcbc3f2b244e
Diniki Jayakody <jayakodyjadn.19@uom.lk>
2024-06-04 11:21:29 +0530

    expert url change

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,8 +1,8 @@
 // config/dev.js
 export default {
-    projectCode: "64fef5352733ffb579bdc92dAVT60UVT8600",
+    projectCode: "64fef5352733ffb579bdc92dAVT120UVT8600",
     baseUrl: "https://cfdpwoutv3.execute-api.ap-south-1.amazonaws.com/dev/",
-    expertUrl: "https://www.lunchbucket.xyz/dev/getReport/",
+    expertUrl: "https://www.lunchbucket.xyz/dev/",
     loginUrl: "https://or5u8idt71.execute-api.ap-south-1.amazonaws.com/beta/"
   };
   
