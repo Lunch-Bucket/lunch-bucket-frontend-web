e72e5a05d028624e87cd2ca1035cdd070af67899
Diniki Jayakody <jayakodyjadn.19@uom.lk>
2024-09-25 21:38:04 +0530

    base url changes

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,8 +1,8 @@
 // config/prod.js
 export default {
     projectCode: "64fdb96f15be57cccddf86bbAVT60UVT8600",
-    baseUrl: "https://r36pslzyv8.execute-api.ap-south-1.amazonaws.com/prod/",
-    expertUrl: "https://www.lunchbucket.xyz/prod/",
-    loginUrl: "https://4mtcsdpx6c.execute-api.ap-south-1.amazonaws.com/prod/"
+    baseUrl: "https://2tbtjpgj61.execute-api.ap-south-1.amazonaws.com/prod/",
+    expertUrl: "http://lunchbucket.xyz:5000/prod/",
+    loginUrl: "https://0ogul0sls2.execute-api.ap-south-1.amazonaws.com/prod/"
   };
   
